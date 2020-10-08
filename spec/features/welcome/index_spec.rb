925ff8ba4031a60465b3e10cff19946cbcefb4ad
Miguel Guardia <guardia@ualberta.ca>
2020-10-08 16:52:22 -0700

    Fix typo

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,10 +4,10 @@ describe "When I first visit the app" do
   it "then I see a welcome message, description, login button, and register button" do
     visit root_path
 
-    expect(current_path).to eq(rooth_path)
+    expect(current_path).to eq(root_path)
     expect(page).to have_content("Welcome to Viewing Party!")
     expect(page).to have_content("The app that lets you set up movie date with your friends!")
     expect(page).to have_button("login")
-    expect(page).to have_button("register")    
+    expect(page).to have_button("register")
   end
 end
