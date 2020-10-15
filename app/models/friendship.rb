159b23af0457bf6d76c6dda91e95a7447bcd02d6
Miguel Guardia <guardia@ualberta.ca>
2020-10-15 12:08:00 -0700

    Complete Issue 21 Dashboard Friends Create new Friendship Controller Create new Friendship Model Add "Friend" and "Friendship" relationship to User model Create partial view for friend search bar Add friend request section to user dashboard Create friendships routes Create spec to test features mentioned

---
+++ app/models/friendship.rb
@@ -0,0 +1,9 @@
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  def self.recipricate_friendship(user_id, friend_id)
+    Friendship.create(user_id: user_id, friend_id: friend_id)
+    Friendship.create(user_id: friend_id, friend_id: user_id)
+  end
+end
--- spec/features/user/index_spec.rb
+++ spec/features/user/index_spec.rb
@@ -12,8 +12,6 @@ RSpec.describe "User Dashboard", type: :feature do
     fill_in :password, with: user.password
     click_on 'login'
 
-    visit users_path
-
     expect(current_path).to eq(users_path)
     expect(page).not_to have_content("The page you were looking for doesn't exist (404)")
   end
@@ -28,7 +26,48 @@ RSpec.describe "User Dashboard", type: :feature do
     visit users_path
 
     expect(page).to have_button('Discover Movies!')
-    expect(page).to have_css(".Friends")
-    expect(page).to have_css(".Party")
+    expect(page).to have_css("#friends")
+    expect(page).to have_css("#party")
+  end
+
+  describe "Should have a friends section" do
+    before :each do
+      @user = create(:user)
+
+      visit new_login_path
+      fill_in :username, with: @user.username
+      fill_in :password, with: @user.password
+      click_on 'login'
+    end
+
+    it "that is empty if you have no friends" do
+      within("#friend-list") do
+        expect(page).to have_content('You currently have no friends')
+      end
+    end
+
+    it "can only add friends if they have a valid email" do
+      @user2 = create(:user)
+
+      within('#friends-search') do
+        fill_in :email, with: @user2.email
+        click_on 'Add Friends'
+      end
+
+      within('#friend-list') do
+        expect(page).to have_content(@user2.username)
+      end
+    end
+
+    it "I can't add friends without an authenticated email" do
+      fake_user = 'nobody@email.com'
+
+      within('#friends-search') do
+        fill_in :email, with: fake_user
+        click_on 'Add Friends'
+      end
+
+      expect(page).to have_content("User does not exist")
+    end
   end
 end
